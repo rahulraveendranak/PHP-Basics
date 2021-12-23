940b0687a70faa682be623f4215a30f6d0ce753e
Rahul Ar <67837177+rahulraveendranak@users.noreply.github.com>
2021-12-23 11:10:09 +0530

    echo print files added

---
+++ echo-print.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>echo-print</title>
+</head>
+<body>
+    <?php
+    echo "<h2> PHP is Fun!</h2>";
+    echo "this ", "string ", "made ", "with multiple parameters.";
+
+
+    $txt1 = "Learn PHP";
+
+    $txt2 = "W3School";
+
+    echo "<h2>".$txt1."</h2>";
+    print "Hello world".$txt1;
+    ?>
+    </body>
+</html>
