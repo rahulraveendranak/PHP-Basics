9ef9ce38cf7eb8436dd75bbe1cb4d82c5c577a05
Rahul A R <rahul.ar@cgtindia.in>
2021-12-23 08:21:36 +0530

    first commit

---
+++ comments.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Comments</title>
+</head>
+<body>
+    <?php 
+    // this is the comment for single line
+
+    #this is also comment for single line
+
+    /* 
+    this is the comment for multiple lines
+    */
+
+    $x = 5 /* + 14 */ + 20 ;
+
+    echo $x;
+
+    ?>
+</body>
+</html>
